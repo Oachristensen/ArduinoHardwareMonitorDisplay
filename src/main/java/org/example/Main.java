16362625af2540ae12daae24d8cc83fe5178d72d
Owen <oachristensen3@gmail.com>
2023-11-15 16:03:53 -0800

    Working prototype done
    Still some stuff I want to optimize but everything works

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,44 +1,52 @@
 package org.example;
 
 
+import jssc.SerialPort;
+
 import java.lang.management.ManagementFactory;
 
+
 public class Main {
+    private static final SerialPort port = new SerialPort("COM3");
+
+    private static final int sleepTimer = 1000;
     public static void main(String[] args) {
-        com.sun.management.OperatingSystemMXBean os = (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
-
-        long physicalMemorySize = os.getTotalMemorySize();
-        long freePhysicalMemory = os.getFreeMemorySize();
-
-        System.out.println(humanReadableByteCount(physicalMemorySize));
-        System.out.println(humanReadableByteCount(freePhysicalMemory));
-        System.out.println(os.getAvailableProcessors());
-        System.out.println(os.getCpuLoad()*100);
+        int systemCpuLoad;
+        int currentCpuLoad = 0;
+        double physicalMemorySize;
+        double freePhysicalMemory;
+        int freeMemoryPercentage;
         try {
-            for (int i = 0; i < 100; i++) {
-                double systemCpuLoad = 0;
-                double currentSystemLoad = 0;
-                Thread.sleep(100);
-                for (int j = 0; j < 5; j++) {
-                    Thread.sleep(100);
-                    currentSystemLoad = os.getCpuLoad();
-                    systemCpuLoad = systemCpuLoad+currentSystemLoad;
-                }
-                systemCpuLoad = systemCpuLoad/5;
-                System.out.println((Math.round(systemCpuLoad*100)) + "%");
+            port.openPort();
+            port.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+
+            com.sun.management.OperatingSystemMXBean os = (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+
+
+            // Main loop
+            while (true) {
+                physicalMemorySize = humanReadableByteCount(os.getTotalMemorySize());
+                freePhysicalMemory = humanReadableByteCount(os.getFreeMemorySize());
+                freeMemoryPercentage = 100 - ((int) Math.round((freePhysicalMemory / physicalMemorySize) * 100));
+
+                systemCpuLoad = (int) Math.round(os.getCpuLoad() * 100);
+
+                // _ is a break char for the serial.read function
+                port.writeString(systemCpuLoad + "% " + freeMemoryPercentage + "%_");
+                Thread.sleep(sleepTimer);
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-// I dont need to store all of these but it makes it more readable for now
+
+    // I dont need to store all of these but it makes it more readable for now
     public static double humanReadableByteCount(long bytes) {
-        long kb = bytes/1024;
-        long mb = kb/1024;
-        double gb =(double) mb/1024;
+        long kb = bytes / 1024;
+        long mb = kb / 1024;
+        double gb = (double) mb / 1024;
         // rounds gb up to 1 decimal place
-        gb = (double) Math.round(gb * 10) /10;
+        gb = (double) Math.round(gb * 10) / 10;
         return gb;
     }
 }
